b3c729fa9ce226d4730f90f7a0d171cc616bdcc5
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-28 11:36:16 -0300

    Refactor button label styling in LoginScreen component

--- src/screens/Auth/LoginScreen.tsx
+++ src/screens/Auth/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, Text, View } from 'react-native';
 
 import BGImg from '~/assets/BGImage.png';
@@ -6,6 +7,7 @@ import { CustomButton } from '~/components';
 export function LoginScreen() {
   return (
     <ImageBackground source={BGImg} className="flex-1 items-center justify-center bg-green-600">
+      <StatusBar hidden />
       <Text className="font-bold text-4xl text-white">SKILL'S</Text>
 
       <Text className="mt-4 max-w-xs text-center text-lg font-semibold text-white">
@@ -13,9 +15,9 @@ export function LoginScreen() {
       </Text>
 
       <View className="mt-7 w-full gap-y-4 p-5">
-        <CustomButton title="Entrar" />
+        <CustomButton title="Cadastre-se no Skill's" />
 
-        <CustomButton title="Entrar" />
+        <CustomButton title="Login" variant="ghost" />
       </View>
     </ImageBackground>
   );
