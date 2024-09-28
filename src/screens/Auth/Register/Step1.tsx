4f33d5df5d74faa147636f65b62241bad1852f53
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-28 13:32:59 -0300

    Refactor Step1 screen to add registration form with email input and continue button

--- src/screens/Auth/Register/Step1.tsx
+++ src/screens/Auth/Register/Step1.tsx
@@ -1,12 +1,31 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
-import { Box } from '~/components';
+import { Box, CustomButton, Header, InfoTerms, InfoText, TextInput } from '~/components';
 
 export function Step1() {
   return (
-    <Box>
-      <View>
-        <Text>Cadastre-se no Skill's</Text>
+    <Box scrollable>
+      <Header />
+
+      <View className="mt-10 flex-1 justify-between">
+        <InfoText text="Cadastre-se no Skill's" />
+
+        <View className="gap-12">
+          <TextInput label="E-mail" placeholder="Digite seu e-mail" />
+
+          <CustomButton title="Continue" variant="secondary" />
+
+          {/* <View className="flex-row items-center justify-center gap-3">
+            <View className="h-px flex-1 bg-gray-300" />
+            <Text className="text-center text-gray-500">ou</Text>
+            <View className="h-px flex-1 bg-gray-300" />
+          </View>
+
+          <CustomButton title="Continue com WhatsApp" variant="disabled" />
+          <CustomButton title="Continue com Celular" variant="disabled" /> */}
+        </View>
+
+        <InfoTerms />
       </View>
     </Box>
   );
