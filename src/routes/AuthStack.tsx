a92899db22eff9128cca3e803b1620d0dc30e891
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-30 07:48:45 -0300

    Refactor AuthStack to include AllowLocation screen

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 
 import { LoginScreen } from '../screens/Auth/LoginScreen';
 
-import { Step1, Step2 } from '~/screens';
+import { StepsScheema } from '~/schemas';
+import { AllowLocation, Step1, Step2 } from '~/screens';
 
 export type AuthStackParamList = {
   LoginScreen: undefined;
   Step1: undefined;
   Step2: { email: string };
+  AllowLocation: { data: StepsScheema };
 };
 
 export function AuthStack() {
@@ -24,6 +26,7 @@ export function AuthStack() {
       <Screen name="LoginScreen" component={LoginScreen} />
       <Screen name="Step1" component={Step1} />
       <Screen name="Step2" component={Step2} />
+      <Screen name="AllowLocation" component={AllowLocation} />
     </Navigator>
   );
 }
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,4 @@
 export * from './Auth/LoginScreen';
+export * from './Auth/Register/AllowLocation';
 export * from './Auth/Register/Step1';
 export * from './Auth/Register/Step2';
