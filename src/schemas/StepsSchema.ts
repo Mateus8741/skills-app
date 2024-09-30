3abc5f1f04492a35dfe55ed6ab5a3ea958b455a4
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-30 07:42:11 -0300

    Refactor password validation in StepsSchema

--- src/schemas/StepsSchema.ts
+++ src/schemas/StepsSchema.ts
@@ -4,7 +4,18 @@ export const stepsScheema = z.object({
   firstName: z.string().min(3, 'O nome deve ter no mínimo 3 caracteres'),
   lastName: z.string().min(3, 'O sobrenome deve ter no mínimo 3 caracteres'),
   email: z.string().email('E-mail inválido'),
-  password: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres'),
+  password: z
+    .string()
+    .min(6)
+    .refine((val) => /[a-z]/.test(val), {
+      message: 'A senha deve conter ao menos uma letra de a-z',
+    })
+    .refine((val) => /\d/.test(val), {
+      message: 'A senha deve conter ao menos um número de 0-9',
+    })
+    .refine((val) => /[!@#$%^&*(),.?":{}|<>]/.test(val), {
+      message: 'A senha deve conter ao menos um caractere especial',
+    }),
 });
 
 export type StepsScheema = z.infer<typeof stepsScheema>;
