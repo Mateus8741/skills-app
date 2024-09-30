3b68c92e584a1b3f919f8aaac4b18fc1f27c67b9
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-30 12:31:11 -0300

    Refactor LocationSchema to make complement and reference fields optional and update houseNumber validation

--- src/schemas/LocationSchema.ts
+++ src/schemas/LocationSchema.ts
@@ -6,17 +6,13 @@ export const locationScheema = z.object({
     .string()
     .min(2, 'Nome do bairro muito curto')
     .max(50, 'Nome do bairro muito longo'),
-  complement: z.string().length(2, 'Complemento muito curto').max(50, 'Complemento muito longo'),
+  complement: z.string().optional(),
+  reference: z.string().optional(),
   houseNumber: z
-    .number()
-    .positive('Número deve ser positivo')
-    .int()
-    .min(1, 'Número da casa deve ser maior que 0'),
-  reference: z
     .string()
-    .length(2, 'Referência muito curta')
-    .max(50, 'Referência muito longa')
-    .optional(),
+    .min(1, 'Número da casa deve ser maior que 0')
+    .transform((value) => Number(value))
+    .refine((value) => value > 0, { message: 'Número da casa deve ser maior que 0' }),
 });
 
 export type LocationScheema = z.infer<typeof locationScheema>;
