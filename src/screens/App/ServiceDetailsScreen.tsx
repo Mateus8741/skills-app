e10fa2020fd92fcbce0ae74d1309182ab488b2bc
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-10-28 16:20:24 -0300

    Refactor ServiceDetailsScreen component: Add CustomButton and update styles

--- src/screens/App/ServiceDetailsScreen.tsx
+++ src/screens/App/ServiceDetailsScreen.tsx
@@ -1,7 +1,7 @@
 import { Star, Timer } from 'lucide-react-native';
 import { ImageBackground, Text, View } from 'react-native';
 
-import { Box, Header, LocationDetails } from '~/components';
+import { Box, CustomButton, Header, LocationDetails } from '~/components';
 import { AppScreenProps } from '~/routes';
 import { calculateInterval, mapImageDetails } from '~/utils';
 
@@ -20,38 +20,51 @@ export function ServiceDetailsScreen({ route }: AppScreenProps<'ServiceDetailsSc
       </ImageBackground>
 
       <Box notpt>
-        <View className="mt-5 gap-1">
-          <Text className="font-bold text-2xl">{details.name}</Text>
+        <View className="flex-1">
+          <View className="mt-5 gap-1">
+            <Text className="font-bold text-2xl">{details.name}</Text>
 
-          <View className="flex-row items-center gap-2">
-            <View className="flex-row items-center gap-1">
-              <Star size={16} color="black" fill="black" />
-              <Text className="text-gray-500">{details.rating}</Text>
-            </View>
+            <View className="flex-row items-center gap-2">
+              <View className="flex-row items-center gap-1">
+                <Star size={16} color="black" fill="black" />
+                <Text className="text-gray-500">{details.rating}</Text>
+              </View>
 
-            <View className="flex-row items-center gap-1">
-              <Timer size={16} color="black" />
-              <Text className="text-gray-500">{interval}</Text>
+              <View className="flex-row items-center gap-1">
+                <Timer size={16} color="black" />
+                <Text className="text-gray-500">{interval}</Text>
+              </View>
             </View>
           </View>
+
+          <View className="mt-5 gap-1">
+            <Text className="font-bold text-xl">Descrição</Text>
+            <Text className="text-gray-500">{details.description}</Text>
+          </View>
+
+          <View className="mt-5 gap-1">
+            <Text className="font-bold text-xl">Preço</Text>
+            <Text className="text-gray-500">{details.price}</Text>
+          </View>
+
+          <View className="mt-5 gap-1">
+            <Text className="font-bold text-xl">Duração</Text>
+            <Text className="text-gray-500">duracation of service</Text>
+          </View>
+
+          <LocationDetails {...details.location} />
         </View>
 
-        <View className="mt-5 gap-1">
-          <Text className="font-bold text-xl">Descrição</Text>
-          <Text className="text-gray-500">{details.description}</Text>
-        </View>
+        <View className="-mx-6 flex-shrink border-t border-gray-300 py-6">
+          <View className="flex-row items-center px-6">
+            <View className="flex-1">
+              <Text className="text-2xl text-gray-500">Preço</Text>
+              <Text className="font-bold text-xl text-black underline">{details.price}</Text>
+            </View>
 
-        <View className="mt-5 gap-1">
-          <Text className="font-bold text-xl">Preço</Text>
-          <Text className="text-gray-500">{details.price}</Text>
+            <CustomButton variant="secondary" title="Agendar" />
+          </View>
         </View>
-
-        <View className="mt-5 gap-1">
-          <Text className="font-bold text-xl">Duração</Text>
-          <Text className="text-gray-500">duracation of service</Text>
-        </View>
-
-        <LocationDetails {...details.location} />
       </Box>
     </>
   );
