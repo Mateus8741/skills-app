50e73e8b57d73f2f8abeeaf6d4ef654453040c21
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-11-06 07:34:49 -0300

    Refactor CategoryServicesScreen: Simplify service filtering logic and improve service count display

--- src/screens/App/CategoryServicesScreen.tsx
+++ src/screens/App/CategoryServicesScreen.tsx
@@ -13,12 +13,7 @@ export function CategoryServicesScreen({
 }: AppScreenProps<'CategoryServicesScreen'>) {
   const { category, name } = route.params;
 
-  // Filtra os serviços baseado na categoria
-  const filteredServices = $SERVICES.filter(
-    (service) => service.category.toLowerCase() === category.toLowerCase()
-  );
-
-  console.log('filteredServices', filteredServices);
+  const filteredServices = $SERVICES.filter((service) => service.category === category);
 
   function handleServicePress(data: ServiceCardProps) {
     navigation.navigate('ServiceDetailsScreen', data);
@@ -32,23 +27,19 @@ export function CategoryServicesScreen({
         {filteredServices.length > 0 ? (
           <>
             <Text className="mb-4 text-base text-gray-600">
-              {filteredServices.length} serviços encontrados
+              {filteredServices.length}{' '}
+              {filteredServices.length === 1 ? 'serviço encontrado' : 'serviços encontrados'}
             </Text>
 
             <FlatList
               data={filteredServices}
-              keyExtractor={(item, index) => String(index)}
+              keyExtractor={(_, index) => String(index)}
               showsVerticalScrollIndicator={false}
               contentContainerStyle={{ paddingBottom: 20 }}
               renderItem={({ item }) => (
                 <ServiceCard service={item} onPress={() => handleServicePress(item)} />
               )}
               ItemSeparatorComponent={() => <View className="h-4" />}
-              ListEmptyComponent={() => (
-                <View className="flex-1 items-center justify-center">
-                  <Text className="text-gray-500">Nenhum serviço encontrado nesta categoria</Text>
-                </View>
-              )}
             />
           </>
         ) : (
