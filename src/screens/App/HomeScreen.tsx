362635dc4062a32f188f1853d48a7d0cba5e1f84
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-11-30 13:29:30 -0300

    Refactor HomeScreen: Integrate useGetServices hook for dynamic service retrieval and remove hardcoded services

--- src/screens/App/HomeScreen.tsx
+++ src/screens/App/HomeScreen.tsx
@@ -3,11 +3,14 @@ import { PlusCircle } from 'lucide-react-native';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
+import { useGetServices } from '~/api/useCases/AppCases/useGetServices';
 import { AllSearch, Box, HeaderHome, MostSearch, ServiceCard } from '~/components';
 import { AppTabScreenProps } from '~/routes';
 import { ServiceCardProps } from '~/schemas';
 
 export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
+  const { data } = useGetServices();
+
   function handleServicePress(data: ServiceCardProps) {
     navigation.navigate('ServiceDetailsScreen', data);
   }
@@ -42,7 +45,7 @@ export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
         </View>
 
         <FlatList
-          data={$SERVICES}
+          data={data}
           keyExtractor={(_, index) => String(index)}
           className="px-2"
           scrollEnabled={false}
@@ -50,126 +53,129 @@ export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
           renderItem={({ item }) => (
             <ServiceCard service={item} onPress={() => handleServicePress(item)} />
           )}
+          ListEmptyComponent={
+            <Text className="mt-4 text-center text-gray-500">Nenhum serviço encontrado</Text>
+          }
         />
       </Box>
     </>
   );
 }
 
-export const $SERVICES: ServiceCardProps[] = [
-  {
-    name: 'Eletricista',
-    price: 'R$ 80,00',
-    rating: 4.5,
-    isAuthenticaded: true,
-    userPhoneNumber: '88999999999',
-    location: {
-      city: 'Barbalha',
-      state: 'CE',
-      street: 'Rua Pinto Madeira',
-      neighborhood: 'Centro',
-      complement: 'complemento',
-      reference: 'referencia',
-      number: 340,
-      latitude: -7.311111,
-      longitude: -39.302222,
-      id: '1',
-      serviceId: '123',
-    },
-    description: 'Descrição do serviço',
-    category: 'ELECTRICIAN',
-    createdAt: new Date().toISOString(),
-  },
-  {
-    name: 'Pintor',
-    price: 'R$ 100,00',
-    rating: 4.5,
-    isAuthenticaded: true,
-    userPhoneNumber: '88999999999',
-    location: {
-      city: 'Barbalha',
-      state: 'CE',
-      street: 'Rua dos Bobos',
-      neighborhood: 'Centro',
-      id: '1',
-      complement: 'complemento',
-      reference: 'referencia',
-      number: 0,
-      latitude: -7.311111,
-      longitude: -39.302222,
-      serviceId: '123',
-    },
-    description: 'Descrição do serviço',
-    category: 'PAINTER',
-    createdAt: '2023-01-01T00:00:00.000Z',
-  },
-  {
-    name: 'Pedreiro',
-    price: 'R$ 120,00',
-    rating: 4.5,
-    isAuthenticaded: true,
-    userPhoneNumber: '88999999999',
-    location: {
-      city: 'Barbalha',
-      state: 'CE',
-      street: 'Rua dos Bobos',
-      neighborhood: 'Centro',
-      id: '1',
-      complement: 'complemento',
-      reference: 'referencia',
-      number: 0,
-      latitude: -7.311111,
-      longitude: -39.302222,
-      serviceId: '123',
-    },
-    description: 'Descrição do serviço',
-    category: 'BRICKLAYER',
-    createdAt: '2022-05-01T00:00:00.000Z',
-  },
-  {
-    name: 'Jardineiro',
-    price: 'R$ 80,00',
-    rating: 4.5,
-    isAuthenticaded: true,
-    userPhoneNumber: '88999999999',
-    location: {
-      city: 'Barbalha',
-      state: 'CE',
-      street: 'Rua dos Bobos',
-      neighborhood: 'Centro',
-      id: '1',
-      complement: 'complemento',
-      reference: 'referencia',
-      number: 0,
-      latitude: -7.311111,
-      longitude: -39.302222,
-      serviceId: '123',
-    },
-    description: 'Descrição do serviço',
-    category: 'GARDENER',
-    createdAt: '2019-07-01T00:00:00.000Z',
-  },
-  {
-    name: 'Outros',
-    price: 'R$ 80,00',
-    rating: 4.5,
-    isAuthenticaded: true,
-    userPhoneNumber: '88999999999',
-    location: {
-      city: 'Barbalha',
-      state: 'CE',
-      street: 'Rua dos Bobos',
-      neighborhood: 'Centro',
-      id: '1',
-      complement: 'complemento',
-      reference: 'referencia',
-      number: 0,
-      latitude: -7.311111,
-      longitude: -39.302222,
-      serviceId: '123',
-    },
-    description: 'Descrição do serviço',
-    category: 'OTHERS',
-    createdAt: '2015-06-01T00:00:00.000Z',
-  },
-];
+// export const $SERVICES: ServiceCardProps[] = [
+//   {
+//     name: 'Eletricista',
+//     price: 'R$ 80,00',
+//     rating: 4.5,
+//     isAuthenticaded: true,
+//     userPhoneNumber: '88999999999',
+//     location: {
+//       city: 'Barbalha',
+//       state: 'CE',
+//       street: 'Rua Pinto Madeira',
+//       neighborhood: 'Centro',
+//       complement: 'complemento',
+//       reference: 'referencia',
+//       number: 340,
+//       latitude: -7.311111,
+//       longitude: -39.302222,
+//       id: '1',
+//       serviceId: '123',
+//     },
+//     description: 'Descrição do serviço',
+//     category: 'ELECTRICIAN',
+//     createdAt: new Date().toISOString(),
+//   },
+//   {
+//     name: 'Pintor',
+//     price: 'R$ 100,00',
+//     rating: 4.5,
+//     isAuthenticaded: true,
+//     userPhoneNumber: '88999999999',
+//     location: {
+//       city: 'Barbalha',
+//       state: 'CE',
+//       street: 'Rua dos Bobos',
+//       neighborhood: 'Centro',
+//       id: '1',
+//       complement: 'complemento',
+//       reference: 'referencia',
+//       number: 0,
+//       latitude: -7.311111,
+//       longitude: -39.302222,
+//       serviceId: '123',
+//     },
+//     description: 'Descrição do serviço',
+//     category: 'PAINTER',
+//     createdAt: '2023-01-01T00:00:00.000Z',
+//   },
+//   {
+//     name: 'Pedreiro',
+//     price: 'R$ 120,00',
+//     rating: 4.5,
+//     isAuthenticaded: true,
+//     userPhoneNumber: '88999999999',
+//     location: {
+//       city: 'Barbalha',
+//       state: 'CE',
+//       street: 'Rua dos Bobos',
+//       neighborhood: 'Centro',
+//       id: '1',
+//       complement: 'complemento',
+//       reference: 'referencia',
+//       number: 0,
+//       latitude: -7.311111,
+//       longitude: -39.302222,
+//       serviceId: '123',
+//     },
+//     description: 'Descrição do serviço',
+//     category: 'BRICKLAYER',
+//     createdAt: '2022-05-01T00:00:00.000Z',
+//   },
+//   {
+//     name: 'Jardineiro',
+//     price: 'R$ 80,00',
+//     rating: 4.5,
+//     isAuthenticaded: true,
+//     userPhoneNumber: '88999999999',
+//     location: {
+//       city: 'Barbalha',
+//       state: 'CE',
+//       street: 'Rua dos Bobos',
+//       neighborhood: 'Centro',
+//       id: '1',
+//       complement: 'complemento',
+//       reference: 'referencia',
+//       number: 0,
+//       latitude: -7.311111,
+//       longitude: -39.302222,
+//       serviceId: '123',
+//     },
+//     description: 'Descrição do serviço',
+//     category: 'GARDENER',
+//     createdAt: '2019-07-01T00:00:00.000Z',
+//   },
+//   {
+//     name: 'Outros',
+//     price: 'R$ 80,00',
+//     rating: 4.5,
+//     isAuthenticaded: true,
+//     userPhoneNumber: '88999999999',
+//     location: {
+//       city: 'Barbalha',
+//       state: 'CE',
+//       street: 'Rua dos Bobos',
+//       neighborhood: 'Centro',
+//       id: '1',
+//       complement: 'complemento',
+//       reference: 'referencia',
+//       number: 0,
+//       latitude: -7.311111,
+//       longitude: -39.302222,
+//       serviceId: '123',
+//     },
+//     description: 'Descrição do serviço',
+//     category: 'OTHERS',
+//     createdAt: '2015-06-01T00:00:00.000Z',
+//   },
+// ];
